f3f6e73d9facccf9129eb6e41f1bf3803ab7b93e
Guillaume Bailleul <guillaume.bailleul@gmail.com>
2026-01-13 14:09:13 +0100

    Initial Commit

---
+++ mltest/mltestApp.swift
@@ -0,0 +1,17 @@
+//
+//  mltestApp.swift
+//  mltest
+//
+//  Created by Guillaume BAILLEUL on 13/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct mltestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ mltestTests/mltestTests.swift
@@ -0,0 +1,17 @@
+//
+//  mltestTests.swift
+//  mltestTests
+//
+//  Created by Guillaume BAILLEUL on 13/01/2026.
+//
+
+import Testing
+@testable import mltest
+
+struct mltestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
